088db92da4a6eac7725ae5190375f12cbb8e2b35
tuoitrevohoc <tuoitrevohoc@me.com>
2016-09-19 06:54:31 +0700

    Initial Commit

---
+++ SwiftParallel/SwiftParallel.h
@@ -0,0 +1,19 @@
+//
+//  SwiftParallel.h
+//  SwiftParallel
+//
+//  Created by Tran Thien Khiem on 9/19/16.
+//  Copyright © 2016 Tran Thien Khiem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftParallel.
+FOUNDATION_EXPORT double SwiftParallelVersionNumber;
+
+//! Project version string for SwiftParallel.
+FOUNDATION_EXPORT const unsigned char SwiftParallelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftParallel/PublicHeader.h>
+
+
